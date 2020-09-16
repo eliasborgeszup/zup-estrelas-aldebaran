4931b05ed1614480cbebf56902ed23b42151f366
unknown <elias.borges@zup.com.br>
2020-09-16 20:53:16 -0300

    Adaptacao da classe Pessoa

--- src/br/com/zup/estrelas/exerciciotres/Agenda.java
+++ src/br/com/zup/estrelas/exerciciotres/Agenda.java
@@ -2,6 +2,7 @@ package br.com.zup.estrelas.exerciciotres;
 
 public class Agenda {
 	public void armazenaPessoa(String nome, int idade, float altura) {
-		private Pessoa pessoas = new Pessoa();
+		Pessoa[] pessoas = new Pessoa[10];
+
 	}
 }
