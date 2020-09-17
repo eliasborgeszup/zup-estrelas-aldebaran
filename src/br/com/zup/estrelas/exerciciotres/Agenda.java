0797ca672316de03cc34dc95f38381a0885339c8
unknown <elias.borges@zup.com.br>
2020-09-16 23:52:19 -0300

    Estruturacao da clase programaPrincipal com estrutura de repiticao e reajustes em metodos de agenda

--- src/br/com/zup/estrelas/exerciciotres/Agenda.java
+++ src/br/com/zup/estrelas/exerciciotres/Agenda.java
@@ -5,36 +5,29 @@ import java.util.Arrays;
 import java.util.Locale;
 
 public class Agenda {
-	Pessoa[] pessoas = new Pessoa[9];
-
+	Pessoa[] pessoas = new Pessoa[10];
+	
 	int contadorPessoa = 0;
-
-	public void armazenaPessoa(String nome, int idade, float altura) {
+	
+	public void armazenaPessoa(String nome, int idade, float altura) {	
 		if (contadorPessoa < 10) {
 			pessoas[contadorPessoa] = new Pessoa();
 
 			pessoas[contadorPessoa].setNome(nome);
 			pessoas[contadorPessoa].setIdade(idade);
 			pessoas[contadorPessoa].setAltura(altura);
-			contadorPessoa += 1;
-		}
-	}
 
-	public void imprimirPessoas() {
-		for (int i = 0; i < pessoas.length; i++) {
-			if (pessoas[i] != null) {
-				System.out.printf("Nome: %s", pessoas[i].getNome());
-				System.out.printf("\nIdade: %d", pessoas[i].getIdade());
-				System.out.printf("\nAltura: %.2f\n\n", pessoas[i].getAltura());
-			}
+			contadorPessoa++;
+
+		} else {
+			System.out.println("Você atingiu o limite maximo");
 		}
 	}
 
 	public void removePessoa(String nome) {
 		Collator collator = Collator.getInstance(new Locale("pt", "BR"));
 		collator.setStrength(Collator.PRIMARY);
-		
-		contadorPessoa = 0;
+
 		for (int i = 0; i < pessoas.length; i++) {
 			if (pessoas[i] != null) {
 				String nomeConvertidoString = String.valueOf(pessoas[i].getNome());
@@ -55,9 +48,7 @@ public class Agenda {
 		for (int i = 0; i < pessoas.length; i++) {
 			if (pessoas[i] != null) {
 				String nomeConvertidoString = String.valueOf(pessoas[i].getNome());
-				
-				System.out.println("01 "+ collator.compare(nomeConvertidoString, nome));
-				
+
 				if (collator.compare(nomeConvertidoString, nome) == 0) {
 					return i;
 				}
@@ -66,5 +57,24 @@ public class Agenda {
 		}
 
 		return -1;
+	}
+
+	public void imprimeAgendas() {
+		for (int i = 0; i < pessoas.length; i++) {
+			if (pessoas[i] != null) {
+				System.out.printf("Nome: %s", pessoas[i].getNome());
+				System.out.printf("\nIdade: %d", pessoas[i].getIdade());
+				System.out.printf("\nAltura: %.2f\n\n", pessoas[i].getAltura());
+			}
+		}
+	}
+
+	public void imprimirPessoa(int posicao) {
+		if (pessoas[posicao] != null) {
+			System.out.printf("Nome: %s", pessoas[posicao].getNome());
+			System.out.printf("\nIdade: %d", pessoas[posicao].getIdade());
+			System.out.printf("\nAltura: %.2f\n\n", pessoas[posicao].getAltura());
+		}
+
 	}
 }
--- src/br/com/zup/estrelas/exerciciotres/ProgramaPrincipal.java
+++ src/br/com/zup/estrelas/exerciciotres/ProgramaPrincipal.java
@@ -3,112 +3,89 @@ package br.com.zup.estrelas.exerciciotres;
 import java.util.Scanner;
 
 public class ProgramaPrincipal {
-	
+
 	public static void armazenaPessoa(Agenda agenda) {
 		Scanner teclado = new Scanner(System.in);
+
 		System.out.print("Digite o nome completo: ");
 		String nome = teclado.nextLine();
-		
+
 		System.out.print("Digite sua idade: ");
 		int idade = teclado.nextInt();
-		
+
 		System.out.print("Digite sua altura: ");
 		float altura = teclado.nextFloat();
 
 		agenda.armazenaPessoa(nome, idade, altura);
 	}
-	
+
 	public static void main(String[] args) {
-		int pergunta = 6;
+		int pergunta = 0;
 		int respostaAgenda = 0;
+		int contador = 0;
+
 		Agenda agenda = new Agenda();
 
-		
-		agenda.armazenaPessoa("Elias Junio 01", 22, 1.85f);
-		agenda.armazenaPessoa("Elias Junio", 23, 1.86f);
-		agenda.armazenaPessoa("Elias Junio", 24, 1.87f);
-		agenda.armazenaPessoa("Elias Junio 04", 25, 1.88f);
-		
-		agenda.imprimirPessoas();
-
-		System.out.println(agenda.pessoas[1].getNome());
-		
-		System.out.println("Posição: " + agenda.buscaPessoa("elias Junio"));
-		agenda.removePessoa("elias junio");
-		agenda.removePessoa("elias junio");
-		
-		if (agenda.buscaPessoa("elias das") == -1) {
-			System.out.println("Não foi possivel localizar");
-		}
-		
-
-		System.out.println(agenda.pessoas[0].getNome());
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
 		Scanner teclado = new Scanner(System.in);
-		while(pergunta != 6) {
-			
+
+		while (pergunta != 6) {
+
 			System.out.println("1 - Armazenar pessoa \n2 - Remover pessoa \n3 - Buscar pessoa "
 					+ "\n4 - Imprime agenda \n5 - Imprime pessoa \n6 - SAIR");
 			System.out.print("Escolha uma opção: ");
 			respostaAgenda = teclado.nextInt();
-			
+
 			switch (respostaAgenda) {
 			case 1: {
-				armazenaPessoa(agenda);
+				if (contador < 11) {
+					armazenaPessoa(agenda);
+					contador++;
+				} else {
+					System.out.println("Você atingiu o limite maximo de pessoas");
+				}
 				break;
 			}
-			
+
 			case 2: {
+				System.out.print("Digite o nome completo que deseja remover: ");
+				String nome = teclado.nextLine();
+
+				agenda.removePessoa(nome);
+				break;
 			}
-			
+
 			case 3: {
+				System.out.print("Digite o nome completo que deseja buscar: ");
+				String nome = teclado.nextLine();
+
+				agenda.buscaPessoa(nome);
+				break;
 			}
-			
+
 			case 4: {
+				agenda.imprimeAgendas();
+				break;
 			}
-			
+
 			case 5: {
+				System.out.print("Digite a posição da pessoa que deseja remover: ");
+				int posicao = teclado.nextInt();
+
+				agenda.imprimirPessoa(posicao);
+				break;
 			}
-			
+
 			case 6: {
 				System.out.println("Obrigado, volte sempre :)");
+				break;
 			}
 			default:
 				System.out.println("Opção invalida");
+				break;
 			}
-			
+
 		}
-		
-		
+
 		teclado.close();
 	}
 
