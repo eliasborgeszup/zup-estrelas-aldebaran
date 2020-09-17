222cd4c2e67e780c7794788fecbbc15789b3c41c
unknown <elias.borges@zup.com.br>
2020-09-17 17:10:03 -0300

    Criacao de classe administrativo e seus atributos

---
+++ src/br/com/zup/estrelas/funcionariosuniversidades/Administrativo.java
@@ -0,0 +1,13 @@
+package br.com.zup.estrelas.funcionariosuniversidades;
+
+public class Administrativo extends Funcionario{
+	private String funcaoAdministrativa;
+	private String senioridade;
+	public Administrativo(String nome, String cpf, int numeroRegistro, String orgaoLotacao, float salario, String funcaoAdministrativa, String senioridade) {
+		super(nome, cpf, numeroRegistro, orgaoLotacao, salario);
+		
+		this.funcaoAdministrativa = funcaoAdministrativa;
+		this.senioridade = senioridade;
+	}
+
+}
